0c94226ae66c4cd5ea6b3093a77d5ee351a1d3b0
kimyougmin <dudals896@gmail.com>
2025-07-16 14:54:01 +0900

    design: icon 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,10 @@ export default {
     theme: {
         extend: {
             colors: {
-                black1: "#1C1C1C"
+                black1: "#1C1C1C",
+                black2: "#171F1B",
+                black3: "#818181",
+                black9: "#E8E8E8"
             },
         },
     },
