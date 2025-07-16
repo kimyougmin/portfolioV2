34a1dd3afcba816e3fa9bb615efc0c57a0b65347
kimyougmin <dudals896@gmail.com>
2025-07-16 11:46:01 +0900

    feat: 초기 설정

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Intro from "./components/Intro.tsx";
+
+function App() {
+
+  return (
+    <div className="w-[430px] h-[932px] top-0">
+      <Intro />
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './index.html',
+        './src/**/*.{js,jsx,ts,tsx}'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                black1: "#1C1C1C"
+            },
+        },
+    },
+    plugins: [
+    ],
+}
