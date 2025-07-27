465ed9d2b7b2e0a0b70814cae5863909f2b94f37
kimyougmin <dudals896@gmail.com>
2025-07-27 15:08:08 +0900

    feat: DockBar URL 추가

--- src/components/molecule/DockBar.tsx
+++ src/components/molecule/DockBar.tsx
@@ -1,16 +1,14 @@
-import React from "react";
-
 const items = [
   { src: "/public/icons/mail.png", label: "Email", href: "mailto:dudals89@gmail.com" },
   { src: "/public/icons/github.png", label: "GitHub", href: "https://github.com/kimyougmin" },
-  { src: "/public/icons/tstory.png", label: "Blog", href: "https://yourblog.com" },
+  { src: "/public/icons/tstory.png", label: "Blog", href: "https://hidden-front.tistory.com/" },
 ];
 
 
 const DockBar = () => {
   return (
     <div className="sticky bottom-4 left-0 w-full z-40 flex justify-center mt-20">
-      <div className="flex items-end gap-4 bg-[#2e2e2e]/70 backdrop-blur-sm rounded-2xl px-6 py-4 shadow-xl">
+      <div className="flex items-end gap-2 bg-[#2e2e2e]/70 backdrop-blur-sm rounded-2xl px-6 py-4 shadow-xl">
         {items.map((item, index) => (
           <a
             key={index}
@@ -23,7 +21,7 @@ const DockBar = () => {
               {item.label}
             </div>
             <img
-              className="w-[82px] h-[82px] transition-transform duration-300 group-hover:scale-110"
+              className="w-[72px] h-[72px] transition-transform duration-300 group-hover:scale-110"
               src={item.src}
               alt={item.label}
             />
